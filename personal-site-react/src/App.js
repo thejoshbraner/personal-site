66ae78bb06f9950f7d1c567e097460711e3bb9a0
Josh Braner <braner.josh@gmail.com>
2022-12-20 12:05:58 -0600

    fixed responsiveness with skill cards

--- personal-site-react/src/App.js
+++ personal-site-react/src/App.js
@@ -1,10 +1,5 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.scss";
-import "typeface-lobster";
-import "typeface-open-sans";
+
 import HomePage from "./pages/HomePage";
-import ContentBlock2 from "./components/ContentBlock2";
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 
--- personal-site-react/src/content/home/Skills.js
+++ personal-site-react/src/content/home/Skills.js
@@ -18,56 +18,58 @@ const Skills = () => {
                     md="8"
                     className="content-block-1 d-flex align-items-center mx-auto py-1 px-5"
                 >
-                    <Col md="5" className="mx-auto">
-                        <Card
-                            className="skill-card"
-                            style={{ backgroundColor: "#212529" }}
-                        >
-                            <CardBody className="d-flex align-items-center">
-                                <FontAwesomeIcon
-                                    icon="fa-brands fa-html5"
-                                    size="5x"
-                                    color="white"
-                                />
-                                <div
-                                    style={{ width: "100%" }}
-                                    className="justify-content-center d-flex"
-                                >
-                                    <h2
-                                        className="mt-2 ms-3"
-                                        style={{ color: "white" }}
+                    <Row className="mx-auto" style={{ width: "100%" }}>
+                        <Col md="6">
+                            <Card
+                                className="skill-card mb-2"
+                                style={{ backgroundColor: "#212529" }}
+                            >
+                                <CardBody className="d-flex align-items-center">
+                                    <FontAwesomeIcon
+                                        icon="fa-brands fa-html5"
+                                        size="5x"
+                                        color="white"
+                                    />
+                                    <div
+                                        style={{ width: "100%" }}
+                                        className="justify-content-center d-flex"
                                     >
-                                        HTML5
-                                    </h2>
-                                </div>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col md="5" className="mx-auto">
-                        <Card
-                            className="skill-card"
-                            style={{ backgroundColor: "#212529" }}
-                        >
-                            <CardBody className="d-flex align-items-center">
-                                <FontAwesomeIcon
-                                    icon="fa-brands fa-square-js"
-                                    size="5x"
-                                    color="white"
-                                />
-                                <div
-                                    style={{ width: "100%" }}
-                                    className="justify-content-center d-flex"
-                                >
-                                    <h2
-                                        className="mt-2 ms-3"
-                                        style={{ color: "white" }}
+                                        <h2
+                                            className="mt-2 ms-3"
+                                            style={{ color: "white" }}
+                                        >
+                                            HTML5
+                                        </h2>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col md="6" className="mx-auto">
+                            <Card
+                                className="skill-card"
+                                style={{ backgroundColor: "#212529" }}
+                            >
+                                <CardBody className="d-flex align-items-center">
+                                    <FontAwesomeIcon
+                                        icon="fa-brands fa-square-js"
+                                        size="5x"
+                                        color="white"
+                                    />
+                                    <div
+                                        style={{ width: "100%" }}
+                                        className="justify-content-center d-flex"
                                     >
-                                        JAVASCRIPT
-                                    </h2>
-                                </div>
-                            </CardBody>
-                        </Card>
-                    </Col>
+                                        <h2
+                                            className="mt-2 ms-3"
+                                            style={{ color: "white" }}
+                                        >
+                                            JAVASCRIPT
+                                        </h2>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
             <Row>
@@ -75,56 +77,58 @@ const Skills = () => {
                     md="8"
                     className="content-block-1 d-flex align-items-center mx-auto py-1 px-5"
                 >
-                    <Col md="5" className="mx-auto">
-                        <Card
-                            className="skill-card"
-                            style={{ backgroundColor: "#212529" }}
-                        >
-                            <CardBody className="d-flex align-items-center">
-                                <FontAwesomeIcon
-                                    icon="fa-brands fa-css3-alt"
-                                    size="5x"
-                                    color="white"
-                                />{" "}
-                                <div
-                                    style={{ width: "100%" }}
-                                    className="justify-content-center d-flex"
-                                >
-                                    <h2
-                                        className="mt-2 ms-3"
-                                        style={{ color: "white" }}
+                    <Row className="mx-auto" style={{ width: "100%" }}>
+                        <Col md="6" className="mx-auto">
+                            <Card
+                                className="skill-card mb-2"
+                                style={{ backgroundColor: "#212529" }}
+                            >
+                                <CardBody className="d-flex align-items-center">
+                                    <FontAwesomeIcon
+                                        icon="fa-brands fa-css3-alt"
+                                        size="5x"
+                                        color="white"
+                                    />{" "}
+                                    <div
+                                        style={{ width: "100%" }}
+                                        className="justify-content-center d-flex"
                                     >
-                                        CSS
-                                    </h2>
-                                </div>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col md="5" className="mx-auto">
-                        <Card
-                            className="skill-card"
-                            style={{ backgroundColor: "#212529" }}
-                        >
-                            <CardBody className="d-flex align-items-center">
-                                <FontAwesomeIcon
-                                    icon="fa-brands fa-react"
-                                    size="5x"
-                                    color="white"
-                                />{" "}
-                                <div
-                                    style={{ width: "100%" }}
-                                    className="justify-content-center d-flex"
-                                >
-                                    <h2
-                                        className="mt-2 ms-3"
-                                        style={{ color: "white" }}
+                                        <h2
+                                            className="mt-2 ms-3"
+                                            style={{ color: "white" }}
+                                        >
+                                            CSS
+                                        </h2>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col md="6" className="mx-auto">
+                            <Card
+                                className="skill-card"
+                                style={{ backgroundColor: "#212529" }}
+                            >
+                                <CardBody className="d-flex align-items-center">
+                                    <FontAwesomeIcon
+                                        icon="fa-brands fa-react"
+                                        size="5x"
+                                        color="white"
+                                    />{" "}
+                                    <div
+                                        style={{ width: "100%" }}
+                                        className="justify-content-center d-flex"
                                     >
-                                        REACT
-                                    </h2>
-                                </div>
-                            </CardBody>
-                        </Card>
-                    </Col>
+                                        <h2
+                                            className="mt-2 ms-3"
+                                            style={{ color: "white" }}
+                                        >
+                                            REACT
+                                        </h2>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
@@ -132,5 +136,5 @@ const Skills = () => {
 };
 
 // try two flex columns within the single strap col instead of rows for the skill cards
-
+// chatgpt is awesome, nest in this way: cont - row - col - row - col col
 export default Skills;
